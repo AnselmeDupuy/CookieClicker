16e730908dedd57bc9fa5ae1e1d073072838ec07
Anselme <anselmedupuy@gmail.com>
2025-11-08 21:01:46 +0100

    start menu display

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from src.Button import Button
+from src.Menu import Menu
 import sys
+
+pygame.init()
 
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 900
 
-pygame.init()
+GAME_STATE = "menu" # game state:  menu // playing // else?
 
 clock = pygame.time.Clock()
 
@@ -14,24 +17,34 @@ pygame.display.set_caption("Cookie Cliker (pour l'instant)")
 
 stop = pygame.image.load("assets/exit_btn.png").convert_alpha()
 
-start_button = Button((WINDOW_WIDTH / 2) - (290 / 2), 250, "assets/start_btn.png")
-stop_button = Button((WINDOW_WIDTH / 2) - (250 / 2), 375 , "assets/exit_btn.png")
+button_list =  []
+
+start_button = Button((WINDOW_WIDTH / 2), 250, "assets/start_btn.png", 0.8)
+stop_button = Button((WINDOW_WIDTH / 2), 375 , "assets/exit_btn.png", 0.8)
+
+menu = Menu()
+
+button_list.append(start_button)
+button_list.append(stop_button)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if start_button.handle_event(event):
+        if start_button.handle_event(event) and GAME_STATE == "menu":
             print(f"Start button clicked")
-        if stop_button.handle_event(event):
+            GAME_STATE = "playing"
+        if stop_button.handle_event(event) and GAME_STATE == "playing":
             print(f"Exit button clicked")
+            GAME_STATE = "menu"
 
-    screen.fill((50,50,50))
-
-    start_button.draw(screen)
-    stop_button.draw(screen)
-
+    if GAME_STATE == "playing":
+        screen.fill((50,50,50))
+        stop_button.draw(screen)
+    elif GAME_STATE == "menu":
+        screen.fill((52,78,91))
+        menu.display_menu(screen, button_list)
 
     pygame.display.flip()
 
--- src/Menu.py
+++ src/Menu.py
@@ -1,7 +1,16 @@
 import pygame
 from src.Button import Button
 #AhDesu
+#initialise le menu et lui donne l'état du jeu
 class Menu:
+    def __init__(self):
+        pass
+
+    # prend le screen pour l'affichage, et une liste de bouton et les affiches
+    def display_menu(self, screen, button_list : list[Button]):
+        for button in button_list:
+            button.draw(screen)        
+
     # import Button
     # import save
     # Start / exit / newgame / load
