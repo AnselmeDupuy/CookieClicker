642487b334f3ac0f7dcafc7017e1e43944d05619
Anselme <anselmedupuy@gmail.com>
2025-11-09 15:16:23 +0100

    added important thing

--- main.py
+++ main.py
@@ -25,9 +25,13 @@ pygame.display.set_caption("Cookie Cliker (pour l'instant)")
 
 # Initialize game objects
 
-start_button = Button((WINDOW_WIDTH / 2), 250, "assets/start_btn.png", 0.8)
+start_button = Button((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2 ) - 75 , "assets/start_btn.png", 0.8)
 
-stop_button = Button((WINDOW_WIDTH / 2), 375 , "assets/exit_btn.png", 0.8)
+stop_button = Button((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2 ) + 75, "assets/exit_btn.png", 0.8)
+
+bg = pygame.image.load("assets/bg.png").convert_alpha()
+bg_rect = bg.get_rect()
+cookie_button = Button((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 3), "assets/cookie3.png")
 
 cookie = Cookie()
 
@@ -59,9 +63,13 @@ while True:
             print(f"Exit button clicked")
             game_state = "menu"
 
+    
+
     if game_state == "playing":
         screen.fill((50,50,50))
+        screen.blit(bg, bg_rect)
         stop_button.draw(screen)
+        cookie_button.draw(screen)
     elif game_state == "menu":
         screen.fill((52,78,91))
         menu.display_menu(screen, buttons)
