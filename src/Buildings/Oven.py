736026023ff83d98d95515d2a29e4dea77087916
lchappart <leo.chappart@coda-student.school>
2025-11-12 14:27:20 +0100

    Create building classes

---
+++ src/Buildings/Oven.py
@@ -0,0 +1,12 @@
+import pygame
+from src.Cookie import Cookie
+import Buildings
+
+class Oven(Buildings):
+    def __init__(self):
+        self.name = "Oven"
+        self.multiplicator = 4
+        self.bonus_per_click = 50
+        self.price = 780
+        self.price_multiplicator = 8
+        self.bonuses_multiplicator = 6
